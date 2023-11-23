345de0767a47b07dab6ea6af495dbe15897aac0c
albertotakito647 <145725153+albertotakito647@users.noreply.github.com>
2023-11-22 23:44:04 -0600

    Create NationalallowSeason.kt

---
+++ NationalallowSeason.kt
@@ -0,0 +1,12 @@
+import interfaces.IPromotion
+
+class NationalallowSeason(city: String): National(city).IPromotion {
+    override val discount = 10
+    override val typeDiscount = 0
+
+
+    override fun getPrice(numDays: Int): Int {
+        val amount = super.getPrice(numDays)
+        return if(amount == 0) 0 else getDiscountPrice(amount)
+    }
+}
